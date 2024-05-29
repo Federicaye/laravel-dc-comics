49c921f61fd5ed62f21d472ddf92f73c65599fdb
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-05-29 17:39:18 +0200

    comic.show

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -3,23 +3,23 @@
 @section('title', 'comics')
 
 @section('content')
-<div class="d-flex flex-wrap">
+<div class="d-flex flex-wrap container container-comic">
 @foreach ($comics as $comic )
 
-<div class="card" style="width: 18rem;">
-  <img src="{{$comic->thumb}}" class="card-img-top" alt="...">
+<div class="comic">
+<a href="{{route('comic.show', $comic->id)}}"><img src="{{$comic->thumb}}" class="card-img-top comic-img" alt="$comic->title"> </a>
   <div class="card-body">
-    <h5 class="{{$comic->title}}">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5 class="card-title">{{$comic->title}}</h5>
+    <div class="description"><p class="card-text">{{$comic->description}}</p></div>
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
+    <li class="list-group-item">{{ $comic->series}}</li>
+    <li class="list-group-item">{{ $comic->type}}</li>
+    <li class="list-group-item">{{ $comic->price}}$</li>
   </ul>
   <div class="card-body">
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+  <a href="#" class="btn btn-primary">Edit</a>
+  <a href="#" class="btn btn-primary">Cancel</a>
   </div>
 </div>
 
--- resources/views/comic/show.blade.php
+++ resources/views/comic/show.blade.php
@@ -1,11 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+@extends('layouts.app')
+@section('title', 'comics')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <img src="{{$comic->thumb}}" alt="{{$comic->title}}">
+        </div>
+        <div class="col-6">
+            <h3>{{$comic->title}}</h3>
+            <h5>{{$comic->series}}</h5>
+            <p>{{$comic->description}}</p>
+            <p>{{$comic->price}}$</p>
+            <p>{{$comic->type}}</p>
+        </div>
+
+    </div>
+
+
+</div>
+@endsection
