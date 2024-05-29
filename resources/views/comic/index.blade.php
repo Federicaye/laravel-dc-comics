f0b20bc518d3b414f832bd25dfc2f6fd489a272c
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-05-29 16:50:21 +0200

    :-/

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -1,21 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+@extends('layouts.app')
+@section('title', 'comics')
 
-<body>
-    @foreach ($comics as $comic)
-        <div>{{$comic->title}}</div>
-        <img src="{{$comic->thumb}}" class="o" alt="">
+@section('content')
+<div class="d-flex flex-wrap">
+@foreach ($comics as $comic )
 
- 
+<div class="card" style="width: 18rem;">
+  <img src="{{$comic->thumb}}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="{{$comic->title}}">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
 
-    @endforeach
-    <div class="y">ciao</div>
-</body>
 
-</html>
+
+@endforeach
+</div>
+@endsection
